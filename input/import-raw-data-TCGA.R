cb4b50a83e5c0e51aad4b45e68ed2f6cd4056af2
Aljoscha Leusmann <aljoscha.leusmann@gmail.com>
2019-10-07 23:20:58 +0200

    shortened munging process for TCGA

--- input/import-raw-data-TCGA.R
+++ input/import-raw-data-TCGA.R
@@ -18,8 +18,6 @@ project_ids <- project_ids[grep("TCGA", project_ids)]
 
 project_ids <- project_ids[order(project_ids)]
 
-project_ids <- as.list(project_ids)
-
 
 # download, save, and prepare summarized experiment for each entity
 #
@@ -41,98 +39,25 @@ ProjectTemplate::cache("raw_tcga", depends = c("project_ids"), {
   }
 )
 
-# Filtering and cleaning --------------------------------------------------
-
-# we need a factor without spaces for DESeq analysis to work
-#
-# loop over every element in the tcga data and make the column for the DESeq
-# analysis
-#
-tcga_raw <- lapply(raw_tcga, race_wo)
-
-
-# DESeqDataSet (DDS) ------------------------------------------------------
 
 # create names for the normalized data sets
 #
 
-tcga_names <- vector("character", length(tcga_raw))
+tcga_names <- vector("character", length(raw_tcga))
 
-for (i in seq_len(names(tcga_raw))) {
-  tcga_names[i] <- paste(strsplit(names(tcga_raw[i]),
+for (i in seq_along(names(raw_tcga))) {
+  tcga_names[i] <- paste(strsplit(names(raw_tcga[i]),
                                   "_")[[1]][2],
                          "se", sep = "_")
 }
 
 
-# create DSeqDataSet of SummarizedExperiments
-#
+# Preprocess TCGA data ----------------------------------------------------
 
-tcga_raw_dds1 <- lapply(
-  tcga_raw,
-  dds_conversion
-)
-
-
-# remove duplicate entries of genes, created by the conversion to a DDS
-#
-
-tcga_raw_dds <- lapply(tcga_raw_dds1, remove_duplicates)
-
-
-# assign names
-#
-
-names(tcga_raw_dds) <- paste(names(tcga_raw), "dds", sep = "_")
-
-
-
-# DGEList -----------------------------------------------------------------
-
-# create a DGEList element out of DESeqDataSet
-#
-
-tcga_raw_dgelist <- lapply(tcga_raw_dds, normalize_tcga)
-
-
-names(tcga_raw_dgelist) <- paste(names(tcga_raw), "dgelist", sep = "_")
-
-
-
-# SummarizedExperiment (se) -----------------------------------------------
-
-# transforming DGEList object to RangedSummarizedExperiment (RSE) via
-# DESeqDataSet
-#
-
-tcga <- vector("list", length(tcga_raw_dgelist))
-
-for (i in seq_len(tcga_raw_dgelist)) {
-  tcga[[i]] <- as.DESeqDataSet(tcga_raw_dgelist[[i]])
-  # log2 transformation of cpm
-  #
-  assays(tcga[[i]])$log2ps_counts <- cpm(
-    tcga_raw_dgelist[[i]]$pseudo.counts,
-    log = TRUE
+tcga <- lapply(
+  raw_tcga,
+  munge_tcga
   )
-  # if patients are not dead assign "days_to_last_follow_up" to the $surv_time
-  #
-  not_dead <- is.na(tcga[[i]]$days_to_death)
-  dead <- !is.na(tcga[[i]]$days_to_death)
-
-  if (any(notDead == TRUE)) {
-    tcga[[i]]$surv_times[not_dead] <- tcga[[i]]$days_to_last_follow_up[not_dead]
-    tcga[[i]]$surv_times[dead] <- tcga[[i]]$days_to_death[dead]
-  }
-  # assign TRUE and FALSE values to surv_events in order to make survival plots
-  #
-  tcga[[i]]$surv_events <- grepl(
-    "dead|deceased",
-    tcga[[i]]$vital_status,
-    ignore.case = TRUE
-  )
-}
-
 
 # assign names earlier created
 #
@@ -141,5 +66,10 @@ names(tcga) <- tcga_names
 
 
 # cache the normalized data sets.
-ProjectTemplate::cache("tcga")
-ProjectTemplate::cache("tcga_names")
+ProjectTemplate::cache("tcga", depends = c("raw_tcga"), {
+  tcga <- lapply(
+    raw_tcga,
+    munge_tcga
+  )
+}
+)
